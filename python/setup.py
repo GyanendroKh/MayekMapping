11d4dcd21bb9548722acd7819bdb607262ca082b
Gyanendro Kh <khgyanendro77@gmail.com>
2020-02-10 10:16:14 +0530

    Fixing the Python library.

--- python/setup.py
+++ python/setup.py
@@ -1,31 +1,25 @@
-from distutils.core import setup
+from setuptools import setup
 
+with open("README", "r") as fh:
+  long_description = fh.read()
 
 setup(
   name='meiteimayek-mapping',
-  packages=['meiteimayek-mapping'],
-  version='0.0.1-beta',
+  packages=['mayek'],
+  version='0.0.1b0',
   license='Apache-2.0',
   description='Utility Functions for managing Meitei Mayek Characters and mapping to E-pao Font.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   author='Gyanendro Kh',
   author_email='khgyanendro77@gmail.com',
   url='https://github.com/GyanendroKh/MayekMapping/tree/master/python',
-  download_url='https://github.com/GyanendroKh/MayekMapping/releases/download/v0.0.1-beta/python.tar.gz',
   keywords=['meiteimayek', 'meitei-mayek', 'mapping', 'utility', 'manipuri'],
-  install_requires=[            
-    'json',
-    'io',
-  ],
   classifiers=[        
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Utility Program',
-    'License :: OSI Approved :: Apache-2.0 License',
+    'License :: OSI Approved :: Apache Software License',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
+    'Operating System :: OS Independent'
   ],
 )
